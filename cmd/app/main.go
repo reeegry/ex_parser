1f56783a48a976062d634a4550922ccd9954f791
Nikita Zuev <6u8654u4@gmail.com>
2024-01-25 18:35:16 +0300

    save ex in map

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,40 +8,96 @@ import (
 )
 
 const (
-	url = "https://hist-ege.sdamgia.ru" // TODO: прочекать все остальные предметы
+	inf  = "inf"
+	hist = "his"
+	url  = "https://*-ege.sdamgia.ru" // TODO: прочекать все остальные предметы
 )
+
+// func subjCheck(url string) string {
+// 	var url_copy string = url
+// 	fmt.Println(url_copy)
+
+// 	return url_copy
+// }
+
+type Answer struct {
+	text string
+	imgs []string
+}
+
+type Exersice struct {
+	exText     string
+	imgs_src   []string
+	answer_ptr *Answer
+}
 
 func main() {
 	c := colly.NewCollector()
+	exrsices := make(map[string]*Exersice)
 
 	c.OnHTML("div.nobreak", func(h *colly.HTMLElement) {
 		txt := strings.ReplaceAll(h.Text, "\u00ad", "")
-		fmt.Println(txt)
-		fmt.Println()
+		problem_id := h.DOM.Find("a").Text()
+
+		imgs := []string{}
 		h.ForEach("img", func(_ int, el *colly.HTMLElement) {
 			if el.Attr("class") != "briefcase" {
 				img_src := el.Attr("src")
 				if img_src[:4] != "http" {
-					fmt.Println(url + img_src)
-				} else {
-					fmt.Println(img_src)
+					img_src = url + img_src
 				}
+				imgs = append(imgs, img_src)
 			}
 
 		})
+		exrsice := &Exersice{
+			exText:     txt,
+			imgs_src:   imgs,
+			answer_ptr: new(Answer),
+		}
+		exrsices[problem_id] = exrsice
 	})
 
 	c.OnHTML("tr.prob_answer", func(h *colly.HTMLElement) {
+		var ans_struct *Answer = new(Answer)
+		var id string
+
 		h.ForEach("td", func(_ int, el *colly.HTMLElement) {
-			if el.Attr("style") != "border:1px solid black;width:25px;text-align:center;padding:2px;" {
-				fmt.Println(el.Text)
+			if el.Attr("style") == "border:1px solid black;text-align:left;padding:2px;text-indent:0" { // ans
+				ans_struct.text = el.Text
+			}
+			id_selection := el.DOM.Find("a")
+			_, ok := id_selection.Attr("href")
+			if ok {
+				id = id_selection.Text()
 			}
 		})
+
+		ans_imgs := []string{}
 		h.ForEach("img", func(_ int, el *colly.HTMLElement) {
-			fmt.Println(el.Attr("src"))
-			fmt.Println(el.Text)
+			ans_imgs = append(ans_imgs, el.Attr("src"))
 		})
+		ans_struct.imgs = ans_imgs
+		if entry, ok := exrsices[id]; ok {
+			entry.answer_ptr = ans_struct
+
+		}
+		fmt.Printf("%p\n", ans_struct)
+
 	})
 
-	c.Visit("https://hist-ege.sdamgia.ru/test?theme=42&print=true")
+	c.Visit("https://ege.sdamgia.ru/test?theme=205&print=true")
+
+	for id, ex := range exrsices {
+		for i := 0; i < 100; i++ {
+			fmt.Printf("-")
+		}
+		fmt.Println()
+		fmt.Println(id)
+		fmt.Println(ex.exText)
+		fmt.Println(ex.imgs_src)
+		fmt.Println(ex.answer_ptr)
+		fmt.Printf("%p\n", ex.answer_ptr)
+	}
+
 }
--- docxParse/docx.go
+++ docxParse/docx.go
@@ -22,30 +22,6 @@ type Exersize struct {
 }
 
 func main() {
-	// fmt.Printf("Preparing new document to write at %s\n", *fileLocation)
-
-	// w := docxlib.New()
-	// // add new paragraph
-	// para1 := w.AddParagraph()
-	// // add text
-	// para1.AddText("test")
-
-	// para1.AddText("test font size").Size(22)
-	// para1.AddText("test color").Color("808080")
-	// para2 := w.AddParagraph()
-	// para2.AddText("test font size and color").Size(22).Color("ff0000")
-
-	// nextPara := w.AddParagraph()
-	// nextPara.AddLink("google", `http://google.com`)
-
-	// f, err := os.Create(*fileLocation)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer f.Close()
-	// w.Write(f)
-	// fmt.Println("Document writen. \nNow trying to read it")
-	// Now let's try to read the file
 	readFile, err := os.Open(*fileLocation)
 	if err != nil {
 		panic(err)
@@ -64,17 +40,6 @@ func main() {
 			if child.Run != nil {
 				fmt.Printf("%s", child.Run.Text.Text)
 			}
-			// if child.Link != nil {
-			// 	id := child.Link.ID
-			// 	text := child.Link.Run.InstrText
-			// 	link, err := doc.References(id)
-			// 	if err != nil {
-			// 		fmt.Printf("\tWe found a link with id %s and text %s without target\n", id, text)
-			// 	} else {
-			// 		fmt.Printf("\tWe've found a new hyperlink with ref %s and the text %s\n", link, text)
-			// 	}
-
-			// }
 		}
 		fmt.Println()
 	}
